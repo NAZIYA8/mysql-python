b3df6f446ac2ff2bf093f164edf93643d7fdf4ab
Naziya <naziyasyeda11@gmail.com>
2021-08-05 01:30:31 +0530

    [Naziya]Add: created a customer table

--- Mysql_indexes/indexes.py
+++ Mysql_indexes/indexes.py
@@ -48,8 +48,37 @@ class Indexes:
         finally:
             self.mydb.close()
 
+    def createTable(self):
+        """
+        Description: 
+            This function is used to create a table in a database.
+        Pararmeter:
+            self is an instance of the object
+        """
+
+        self.create_connection()
+        try:
+            myCursor = self.mydb.cursor()
+            customer_record ='''CREATE TABLE customer(
+                                customer_id int not null,
+                                customer_name varchar(150) not null,
+                                customer_city varchar(150),
+                                customer_country varchar(150),
+                                PRIMARY KEY (customer_id)
+                                )'''
+            myCursor.execute("DROP TABLE IF EXISTS customer")
+            myCursor.execute(customer_record)
+            myCursor.execute("SHOW TABLES")
+            for tb in myCursor:
+                print(tb)
+        except Exception as err:
+            logger.error(err)
+        finally:
+            self.mydb.close()
+
 
 if __name__ == "__main__":
     indexes = Indexes()
     indexes.create_database()
-    
+    indexes.createTable()
+    print("Successfully created table")
