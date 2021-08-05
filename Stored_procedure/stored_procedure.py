4e0baa6462d66ab0f4bf0d5400545122c6814df4
Naziya <naziyasyeda11@gmail.com>
2021-08-06 02:17:11 +0530

    [Naziya]Add: showed data inserted into table

--- Stored_procedure/stored_procedure.py
+++ Stored_procedure/stored_procedure.py
@@ -66,7 +66,7 @@ class StoredProcedure:
                                 student_name varchar(150) not null,
                                 subject varchar(150),
                                 marks int,
-                                phone int(10),
+                                phone varchar(45),
                                 PRIMARY KEY (student_id)
                                 )'''
             myCursor.execute("DROP TABLE IF EXISTS student_info")
@@ -84,16 +84,16 @@ class StoredProcedure:
         Description: 
             This function is used to insert data into a database
         Pararmeter:
-            self is an instance of the objecte
+            self is an instance of the object
         """
 
         self.create_connection()
         try:
             myCursor = self.mydb.cursor()
             insert_formula = "INSERT into student_info (student_id,student_code, student_name,subject,marks,phone) VALUES (%s,%s,%s,%s,%s,%s)"
-            insert_records = [(1,101,'Kinjal','English',68,9875842658),(2,102,'Dolly','physics',70,9365842658),(3,103,'Diana','maths',70,9275844858),
-                               (4,104,'vanraj','maths',90,9375844658),(5,105,'komal','maths',85,9875841658),(6,106,'dhiraj','science',92,9775849658),
-                               (7,107,'piyush','science',83,9875847658),(8,108,'sanjay','science',85,7275842658),(9,109,'prachi','Biology',67,8375832658)]
+            insert_records = [(1,101,'Kinjal','English',68,'9875842651'),(2,102,'Dolly','physics',70,'9365842652'),(3,103,'Diana','maths',70,'9275844854'),
+                               (4,104,'vanraj','maths',90,'9237584465'),(5,105,'komal','maths',85,'9875841655'),(6,106,'dhiraj','science',92,'9577584965'),
+                               (7,107,'piyush','science',83,'9875847654'),(8,108,'sanjay','science',85,'7275842657'),(9,109,'prachi','Biology',67,'8375832651')]
             myCursor.executemany(insert_formula,insert_records)
             self.mydb.commit()
         except Exception as err:
@@ -101,8 +101,29 @@ class StoredProcedure:
         finally:
             self.mydb.close()
 
+    def show_data(self):
+        """
+        Description: 
+            This function is used to show the data.
+        Pararmeter:
+            self is an instance of the object
+        """
+
+        self.create_connection()
+        try:
+            myCursor = self.mydb.cursor()    
+            myCursor.execute("SELECT * from student_info")
+            result_set = myCursor.fetchall()
+            for data in result_set:
+                print(data)
+        except Exception as err:
+            logger.error(err)
+        finally:
+            self.mydb.close()
 
 if __name__ == "__main__":
     storedProcedure = StoredProcedure()
     storedProcedure.create_database()
     storedProcedure.createTable()
+    storedProcedure.insert_data()
+    storedProcedure.show_data()
