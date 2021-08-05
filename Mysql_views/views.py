fe2307760f3233f24698eab789bd1e275643b94f
Naziya <naziyasyeda11@gmail.com>
2021-08-05 20:21:40 +0530

    [Naziya]Add: created a table and inserted the data

--- Mysql_views/views.py
+++ Mysql_views/views.py
@@ -49,7 +49,60 @@ class Views:
             self.mydb.close()
 
 
+    def createTable(self):
+        """
+        Description: 
+            This function is used to create a table in a database.
+        Pararmeter:
+            self is an instance of the object
+        """
+
+        self.create_connection()
+        try:
+            myCursor = self.mydb.cursor()
+            employee_record ='''CREATE TABLE employees(
+                                employee_id int not null,
+                                employee_name varchar(150) not null,
+                                employee_country varchar(150),
+                                employee_age int,
+                                employee_salary int(10)
+                                PRIMARY KEY (employee_id)
+                                )'''
+            myCursor.execute("DROP TABLE IF EXISTS employees")
+            myCursor.execute(employee_record)
+            myCursor.execute("SHOW TABLES")
+            for tb in myCursor:
+                print(tb)
+        except Exception as err:
+            logger.error(err)
+        finally:
+            self.mydb.close()
+
+    def insert_data(self):
+        """
+        Description: 
+            This function is used to insert data into a database
+        Pararmeter:
+            self is an instance of the object
+        """
+
+        self.create_connection()
+        try:
+            myCursor = self.mydb.cursor()
+            insert_formula = "INSERT into employee (employee_id, employee_name,employee_country,employee_age,employee_salary) VALUES (%s,%s,%s,%s,%s)"
+            insert_records = [  (1,'Kinjal','mumbai',25,18000,),
+                                (2,'Dolly','Bangalore',28,22000),
+                                (3,'John','Delhi',30,32000),
+                                (4,'Tom','Hyderabad',24,20000)]
+            myCursor.executemany(insert_formula,insert_records)
+            self.mydb.commit()
+        except Exception as err:
+            logger.error(err)
+        finally:
+            self.mydb.close()
 
 if __name__ == "__main__":
     views = Views()
     views.create_database()
+    views.createTable()
+    views.insert_data()
